27e6a55fe9724dac5ab63396e081f38bc0c0c572
Baruch Rothkoff <baruchiro@gmail.com>
2019-11-20 17:22:28 +0200

    First commit

---
+++ DllProps.Tool/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DllProps.Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var info = FileVersionInfo.GetVersionInfo(args[0]);
+            Console.WriteLine($"Company: {info.CompanyName}");
+            Console.WriteLine($"Copyright: {info.LegalCopyright}");
+            Console.WriteLine($"FileVersion: {info.FileVersion}");
+            Console.WriteLine($"ProductVersion: {info.ProductVersion}");
+            Console.WriteLine($"Description: {info.FileDescription}");
+            Console.WriteLine($"Product: {info.}");
+        }
+    }
+}
